094bbcec82baa51a1e55ed550b35afd213e177db
unknown <nk-nishizawa@NK-NISHIZAWA.sgi.co.jp>
2012-07-02 18:56:41 +0900

    commit for sharing

--- js/nbody.js
+++ js/nbody.js
@@ -2,13 +2,53 @@
  * @author nk.nishizawa@gmail.com
  */
 
-var a="window.a";
-var obj = {
-	a : "obs.a",
-	func : 
-		function () {
-			alert(a);
-		}
+var V3 = function (x, y, z) {
+	this.x = x;
+	this.y = y;
+	this.z = z;
+}
+
+V3.prototype.add = function (v) {
+	this.x += v.x;
+	this.y += v.y;
+	this.z += v.z;
+	return this;
 };
 
-obj.func();
+V3.prototype.sub = function (v) {
+	this.x -= v.x;
+	this.y -= v.y;
+	this.z -= v.z;
+	return this;
+};
+
+V3.prototype.mul = function (s) {
+	this.x *= s;
+	this.y *= s; 
+	this.z *= s;
+	return this;
+};
+
+Vector3.dot = function (v0, v1) {
+	return v0[0]*v1[0] + v0[1]*v1[1] + v0[2]*v1[2];
+};
+
+Vector3.length = function (v) {
+	var x = v[0]; var y = v[1]; var z = v[2];
+	return Math.sqrt(x*x + y*y + z*z);
+};
+
+Vector3.length2 = function (v) {
+	var x = v[0]; var y = v[1]; var z = v[2];
+	return x*x + y*y + z*z;
+};
+
+Vector3.normalize = function (v) {
+	var x = v[0]; var y = v[1]; var z = v[2];
+	var s = 1.0 / Math.sqrt(x*x + y*y + z*z)
+	var dst = new VECTOR_TYPE(3);
+	dst[0] = s * x;
+	dst[1] = s * y;
+	dst[2] = s * z;
+	return dst;
+};
